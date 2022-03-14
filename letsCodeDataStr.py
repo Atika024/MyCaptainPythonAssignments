3724534f85ccef2f2196f05d82807838d4c9e7a0
Atika Biswas <82339369+Atika024@users.noreply.github.com>
2022-03-14 21:36:33 +0530

    Add files via upload

---
+++ letsCodeDataStr.py
@@ -0,0 +1,30 @@
+"""
+Task 1: Write a Python program which accepts the radius of a circle from the user and computes area.
+"""
+
+from msilib.schema import Extension
+from typing import Dict
+
+
+PI = 3.14
+r = float (input("Input the radius of the circle: "))
+area = PI*r*r
+print ("The area of the circle with radius ", r, " is: ", area)
+
+
+"""
+Task 2: Write a Python program to accept a filename from the user and print the extension of that.
+"""
+
+Dict = {
+    "abc.py":"Python",
+    "abc.c":"C",
+    "abc.cpp":"C++",
+    "abc.html":"HTML",
+    "abc.js":"Javascript",
+    "abc.java":"Java",
+    "abc.css":"CSS"
+}
+
+ext = input ("Enter the filename: abc.")
+print ("The extension of the file is: ", Dict["abc."+ext])
